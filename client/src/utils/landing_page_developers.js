aab045f60d463ef7f270c420c41193f9628403da
Munendra-bhadouriya <munendra.connect@gmail.com>
2025-05-24 01:09:17 +0530

    final commit

---
+++ client/src/utils/landing_page_developers.js
@@ -0,0 +1,55 @@
+const Images = [
+    {
+        id: 0,
+        src: "/img/landing_page_images/features-01.png",
+        title: "feature1",
+    },
+    {
+        id: 1,
+        src: "/img/landing_page_images/features-02.png",
+        title: "feature2",
+    },
+    {
+        id: 2,
+        src: "/img/landing_page_images/features-03.png",
+        title: "feature3",
+    },
+    {
+        id: 3,
+        src: "/img/landing_page_images/dev-01.jpg",
+        title: "Keshav Meena",
+        role: "FullStack Dev",
+        linkedinUrl: "",
+        githubUrl: "",
+        websiteUrl: "",
+    },
+    {
+        id: 4,
+        src: "/img/landing_page_images/dev-02.jpg",
+        title: "Harsh Kumar",
+        role: "FullStack Dev",
+        linkedinUrl: "",
+        githubUrl: "",
+        websiteUrl: "",
+    },
+    {
+        id: 5,
+        src: "/img/landing_page_images/dev-03.jpg",
+        title: "Prashant Tripathi",
+        role: "FullStack Dev",
+        linkedinUrl: "",
+        githubUrl: "",
+        websiteUrl: "",
+    },
+    {
+        id: 6,
+        src: "/img/landing_page_images/pm-01.jpg",
+        title: "Khusboo Gupta",
+        role: "FullStack Dev",
+        linkedinUrl: "",
+        githubUrl: "",
+        websiteUrl: "",
+    },
+];
+
+export default Images;
